7e33d8d2b6d485e180ca3b68c60a3f088318814b
Kevin Semple <KSemp@falconpunch.local>
2017-08-20 12:18:02 -0400

    Fixed merging issue

--- pong/Ball.cpp
+++ pong/Ball.cpp
@@ -11,11 +11,22 @@
 #include <time.h>
 #include "Globals.h"
 
-Ball::Ball(float startX, float startY){
-    position.x = startX;
-    position.y = startY;
-    xVelocity = 50.0;
-    yVelocity = 50.0;
+Ball::Ball(){
+    reset();
+}
+
+void Ball::reset(){
+    srand(time(NULL));
+    int direction;
+    int direction2;
+    position.x = rand() % 100 + ((int)WINDOW_WIDTH/2) - 50;//modulo range + inital value
+    position.y = rand() % (int)WINDOW_HEIGHT/2 + 1;//modulo range + inital value
+    velocity.x = rand() % 10 + 5.2;
+    velocity.y = rand() % 10 + 5.2;
+    direction = rand() % 10 + 1;
+    direction2 = rand() % 10 + 1;
+    if(direction>5){velocity.x*=-1;}
+    if(direction2>5){velocity.y*=-1;}
     
     ballShape.setSize(sf::Vector2f(10, 10));
     ballShape.setOutlineColor(sf::Color::White);
@@ -36,26 +47,27 @@ sf::Vector2f Ball::getVelocity() const{
 
 //TODO: Implement swept path collision detection
 void Ball::reboundTopOrBottom(){
-    //position.y -= (yVelocity * 30);
-    yVelocity = -yVelocity;
+    //position.y -= (velocity.y * 1);
+    //velocity.y++;
+    velocity.y = -(velocity.y);
 }
 
 
 void Ball::reboundBat(){
-    //position.x -= (xVelocity * 30);
-    xVelocity = -xVelocity;
+    //position.x -= (velocity.x * 1);
+    //velocity.x++;
+    //velocity.y++;
+    velocity.x = -(velocity.x*1.05);
 }
-    
+
 void Ball::hitSides(){
     reset();
 }
 
-void Ball::update(float elapsed){
-    //position.x += xVelocity;
-    //position.y += xVelocity;
+void Ball::update( ){
+    //position += velocity;
     //ballShape.setPosition(position);
-    ballShape.setPosition(position.x+elapsed*xVelocity,position.y+elapsed*xVelocity);
-    ballShape.move(elapsed*xVelocity,elapsed*xVelocity);
+    ballShape.move(velocity);
 }
 
 
